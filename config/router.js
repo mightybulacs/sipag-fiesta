22858e2ddeadf234af1ce3b19163789ecaf9a547
eleimeguiso <eleiimeguiso@gmail.com>
2016-07-04 17:09:47 +0800

    add temporary route for test controller

---
+++ config/router.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const config    = require(__dirname + '/../config/config');
+const importer  = require('anytv-node-importer');
+
+
+module.exports = (router) => {
+
+  const __ = importer.dirloadSync(__dirname + '/../controllers');
+
+  router.del = router.delete;
+
+  router.get ('/test', __.test.get_test);
+
+  router.all('*', (req, res) => {
+    res.status(404)
+      .send({message: 'Nothing to do here.'});
+  });
+
+  return router;
+};
