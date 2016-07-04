addf592ee58fa9a1b76535eded4c22451dc36597
eleimeguiso <eleiimeguiso@gmail.com>
2016-07-04 17:09:17 +0800

    add production environment configuration file

---
+++ config/env/production.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const proc_id = process.env.cpu_number || 0;
+
+module.exports = {
+    ENV: 'production',
+
+    CORS:  {
+        allowed_headers: 'Content-Type, Accept',
+        allowed_origins_list: [
+            'sipag-fiesta.loc'
+        ],
+        allowed_methods: 'GET, POST, PUT, DELETE',
+        allow_credentials: true
+    },
+
+    MASTER_DB: {
+        host: 'localhost',
+        user: 'sipag',
+        password: 'pwds1p4g',
+        database: 'sipagdb',
+        connectionLimit: 200
+    },
+
+    SLAVE_DB: {
+        host: 'localhost',
+        user: 'sipag',
+        password: 'pwds1p4g',
+        database: 'sipagdb',
+        connectionLimit: 200
+    }
+}
