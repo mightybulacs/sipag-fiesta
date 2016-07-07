98e9e0b3a46b3973fe9d4f3210632ece4348a55d
aacarrasco <aacarrasco@up.edu.ph>
2016-07-07 14:22:47 +0800

    edited router.js

--- config/router.js
+++ config/router.js
@@ -11,13 +11,19 @@ module.exports = (router) => {
   router.del = router.delete;
 
   router.get ('/test',          						__.test.get_test);
-  router.get ('/commodities',    						__.commodity.get_test);
 
   //CATEGORY
   router.get ('/categories',    						__.category.get_category);
   router.post ('/categories', 							__.category.post_category);
   router.put ('/categories/:name', 						__.category.put_category);
   router.delete ('/categories/:name', 					__.category.delete_category);
+
+  //COMMODITY
+  router.get('/commodities', 							__.commodity.get_commodity);
+  router.get('/:category/commodities',				 	__.commodity.get_commodity_category);
+  router.post('/commodities', 							__.commodity.post_commodity);
+  router.put('/commodities/:id', 						__.commodity.put_commodity);
+  router.delete('/commodities/:id', 					__.commodity.delete_commodity);
 
   //BENEFIT
   router.get ('/technologies/:id/benefits',   			__.benefit.get_benefit);
@@ -32,16 +38,16 @@ module.exports = (router) => {
   router.delete ('/objectives/:id', 					__.objective.delete_objective);
 
   //BENEFICIARY
-  router.get ('/technologies/:id/beneficiaries',    __.beneficiary.get_beneficiary);
-  router.post ('/technologies/:id/beneficiaries',   __.beneficiary.post_beneficiary);
-  router.put ('/beneficiaries/:id',  __.beneficiary.put_beneficiary);
-  router.delete ('/beneficiaries/:id',   __.beneficiary.delete_beneficiary);
+  router.get ('/technologies/:id/beneficiaries',    	__.beneficiary.get_beneficiary);
+  router.post ('/technologies/:id/beneficiaries',   	__.beneficiary.post_beneficiary);
+  router.put ('/beneficiaries/:id',  					__.beneficiary.put_beneficiary);
+  router.delete ('/beneficiaries/:id',   				__.beneficiary.delete_beneficiary);
 
   //PARTNER
-  router.get ('/technologies/:id/partners',    __.partner.get_partner);
-  router.post ('/technologies/:id/partners',   __.partner.post_partner);
-  router.put ('/partners/:id',  __.partner.put_partner);
-  router.delete ('/partners/:id',   __.partner.delete_partner);
+  router.get ('/technologies/:id/partners',    			__.partner.get_partner);
+  router.post ('/technologies/:id/partners',   			__.partner.post_partner);
+  router.put ('/partners/:id',  						__.partner.put_partner);
+  router.delete ('/partners/:id',   					__.partner.delete_partner);
 
 
   router.all('*', (req, res) => {
