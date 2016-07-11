9ad94674361048789af4108825131d21531c9116
eleimeguiso <eleiimeguiso@gmail.com>
2016-07-11 18:44:09 +0800

    Edited router.js (page number and size from params to query)

--- config/router.js
+++ config/router.js
@@ -13,53 +13,53 @@ module.exports = (router) => {
   router.get ('/test',                                          __.test.get_test);
 
   //BENEFICIARY
-  router.get ('/technologies/:id/beneficiaries/:page/:size',    __.beneficiary.get_beneficiary);
+  router.get ('/technologies/:id/beneficiaries',                __.beneficiary.get_beneficiary);
   router.post('/technologies/:id/beneficiaries',                __.beneficiary.post_beneficiary);
   router.put ('/beneficiaries/:id',                             __.beneficiary.put_beneficiary);
   router.del ('/beneficiaries/:id',                             __.beneficiary.delete_beneficiary);
 
   //BENEFIT
-  router.get ('/technologies/:id/benefits/:page/:size',         __.benefit.get_benefit);
+  router.get ('/technologies/:id/benefits',                     __.benefit.get_benefit);
   router.post('/technologies/:id/benefits',                     __.benefit.post_benefit);
   router.put ('/benefits/:id',                                  __.benefit.put_benefit);
   router.del ('/benefits/:id',                                  __.benefit.delete_benefit);
 
   //CATEGORY
-  router.get ('/categories/:page/:size',                        __.category.get_category);
+  router.get ('/categories',                                    __.category.get_category);
   router.post('/categories',                                    __.category.post_category);
   router.put ('/categories/:name',                              __.category.put_category);
   router.del ('/categories/:name',                              __.category.delete_category);
 
   //COMMODITY
-  router.get ('/commodities/:page/:size',                       __.commodity.get_commodity);
-  router.get ('/:category/commodities/:page/:size',             __.commodity.get_commodity_category);
+  router.get ('/commodities',                                   __.commodity.get_commodity);
+  router.get ('/:category/commodities',                         __.commodity.get_commodity_category);
   router.post('/commodities',                                   __.commodity.post_commodity);
   router.put ('/commodities/:id',                               __.commodity.put_commodity);
   router.del ('/commodities/:id',                               __.commodity.delete_commodity);
 
   //LOCATION
-  router.get ('/technologies/:id/locations/:page/:size',        __.location.get_location);
+  router.get ('/technologies/:id/locations',                    __.location.get_location);
   router.post('/technologies/:id/locations',                    __.location.post_location);
   router.put ('/locations/:id',                                 __.location.put_location);
   router.del ('/locations/:id',                                 __.location.delete_location);
 
   //OBJECTIVE
-  router.get ('/technologies/:id/objectives/:page/:size',       __.objective.get_objective);
+  router.get ('/technologies/:id/objectives',                   __.objective.get_objective);
   router.post('/technologies/:id/objectives',                   __.objective.post_objective);
   router.put ('/objectives/:id',                                __.objective.put_objective);
   router.del ('/objectives/:id',                                __.objective.delete_objective);
 
   //PARTNER
-  router.get ('/technologies/:id/partners/:page/:size',         __.partner.get_partner);
+  router.get ('/technologies/:id/partners',                     __.partner.get_partner);
   router.post('/technologies/:id/partners',                     __.partner.post_partner);
   router.put ('/partners/:id',                                  __.partner.put_partner);
   router.del ('/partners/:id',                                  __.partner.delete_partner);
 
   //TECHNOLOGY
   router.get ('/technologies/:id',                              __.technology.get_oneTechnology);
-  router.get ('/technologies/:page/:size',                      __.technology.get_technology);
-  router.get ('/category/:category/technologies/:page/:size',            __.technology.get_category_technology);
-  router.get ('/commodity/:commodity/technologies/:page/:size',           __.technology.get_commodity_technology);
+  router.get ('/technologies/',                                 __.technology.get_technology);
+  router.get ('/category/:category/technologies',               __.technology.get_category_technology);
+  router.get ('/commodity/:commodity/technologies',             __.technology.get_commodity_technology);
   router.post('/technologies',                                  __.technology.post_technology);
   router.put ('/technologies/:id',                              __.technology.put_technology);
   router.del ('/technologies/:id',                              __.technology.delete_technology);     
